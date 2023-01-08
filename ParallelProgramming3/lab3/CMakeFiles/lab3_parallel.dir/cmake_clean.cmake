66fea3fef313080a4cb530b4375b28957b97a394
Bolychev Leonid <transcendentalL@yandex.ru>
2023-01-08 23:07:35 +1000

    second commit

---
+++ ParallelProgramming3/lab3/CMakeFiles/lab3_parallel.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab3_parallel.dir/main.cpp.obj"
+  "CMakeFiles/lab3_parallel.dir/main.cpp.obj.d"
+  "lab3_parallel.exe"
+  "lab3_parallel.exe.manifest"
+  "lab3_parallel.pdb"
+  "liblab3_parallel.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab3_parallel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ParallelProgramming3/lab3/CMakeFiles/laba3_parallel.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laba3_parallel.dir/main.cpp.obj"
+  "CMakeFiles/laba3_parallel.dir/main.cpp.obj.d"
+  "laba3_parallel.exe"
+  "laba3_parallel.exe.manifest"
+  "laba3_parallel.pdb"
+  "liblaba3_parallel.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/laba3_parallel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
